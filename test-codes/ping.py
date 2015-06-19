19591bb3c45d467bed85edf36c237146cec10eb7
Rishabh Jain <rishabja@cisco.com>
2015-06-19 11:53:15 -0700

    Ping exit codes

--- test-codes/ping.py
+++ test-codes/ping.py
@@ -5,11 +5,11 @@ import sys
 if len(sys.argv) == 3:
 	p = Popen(["nping", "-c1" ,"-p", sys.argv[2],sys.argv[1]], stdout=PIPE)
 else:
-	p = Popen(["nping", "-c1" ,sys.argv[1]], stdout=PIPE)
+	p = Popen(["ping", "-c1" ,sys.argv[1]], stdout=PIPE)
 
 fail = False
 for line in p.stdout:
-	if "refused" in line:
+	if "refused" in line or "Unreachable" in line:
 		fail = True
 
 if (fail):
