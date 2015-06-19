fad663a90d183a7c3c6a5fb7141fc8e977d315c1
Rishabh Jain <rishabja@cisco.com>
2015-06-19 11:42:53 -0700

    Ping exit codes corrected

--- test-codes/ping.py
+++ test-codes/ping.py
@@ -1,7 +1,12 @@
 from subprocess import Popen, PIPE, STDOUT
 import random
 import sys
-p = Popen(["nping", "-c1" ,"-p", sys.argv[2],sys.argv[1]], stdout=PIPE)
+
+if len(sys.argv) == 3:
+	p = Popen(["nping", "-c1" ,"-p", sys.argv[2],sys.argv[1]], stdout=PIPE)
+else:
+	p = Popen(["nping", "-c1" ,sys.argv[1]], stdout=PIPE)
+
 fail = False
 for line in p.stdout:
 	if "refused" in line:
